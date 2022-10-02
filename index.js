278251e1dff5c85993dbfb8275e5c6060241b56c
kakotetsuoml <114625062+kakotetsuoml@users.noreply.github.com>
2022-10-02 12:34:45 +0900

    Add files via upload

---
+++ index.js
@@ -0,0 +1,37 @@
+$(function () {
+    // カレンダー
+    $(function () {
+        $('input[name="date"]').datepicker({
+            dateFormat: 'yy/mm/dd',
+        });
+    });
+
+    // 参加人数分の氏名欄を生成
+    $('#form-number').click(function () {
+        $('#form-name').empty();
+        var num = $('input[name="number"]:checked').val();
+        for (i = 0; i < num; i++) {
+            $('#form-name').append(
+                `<input class="form-control w-100 mt-1" name="name" maxlength="10">`
+            );
+        }
+    });
+
+    // 送信
+    $('form').submit(function () {
+        var date = $('input[name="date"]').val();
+        var event = $('input[name="event"]').val();
+         var kyojuuku = $('input[name="kyojuuku"]').val();
+        var number = $('input[name="number"]:checked').val();
+        var names = '';
+        $('#form-name').children().each(function (i, elm) {
+            names += $(elm).val() + '、';
+        })
+        names = names.slice(0, -1);
+
+        var msg = `行事日：${date}\n行事名：${event}\n人数：${number}\n氏名：${names}\n居住区：${kyojuuku}`;
+        sendText(msg);
+
+        return false;
+    });
+});
